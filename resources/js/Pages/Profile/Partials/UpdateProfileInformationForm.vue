c6cc531e08ae7f7e7b8b7e800dacbd24821b8b48
PROPHET <jorgeluis33ariosa@gmail.com>
2023-01-23 18:18:54 -0500

    first commit

---
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -0,0 +1,145 @@
+<script setup>
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Link, useForm, usePage } from "@inertiajs/vue3";
+
+const props = defineProps({
+    mustVerifyEmail: Boolean,
+    status: String,
+});
+
+const user = usePage().props.auth.user;
+
+let date = new Date(user.birthday);
+let options = { year: "numeric", month: "2-digit", day: "2-digit" };
+
+const data = {
+    mydate: "2017-07-04",
+};
+
+const form = useForm({
+    name: user.name,
+    lastname: user.lastname,
+    address: user.address,
+    birthday: date.toLocaleDateString("en-CA"),
+});
+</script>
+
+<template>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900">
+                Profile Information
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600">
+                Update your account's profile information.
+            </p>
+        </header>
+
+        <form
+            @submit.prevent="form.patch(route('profile.update'))"
+            class="mt-6 space-y-6"
+        >
+            <div>
+                <InputLabel for="name" value="Name" />
+
+                <TextInput
+                    id="name"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.name"
+                    required
+                    autofocus
+                    autocomplete="name"
+                />
+
+                <InputError class="mt-2" :message="form.errors.name" />
+            </div>
+
+            <div>
+                <InputLabel for="lastname" value="lastname" />
+
+                <TextInput
+                    id="lastname"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.lastname"
+                    required
+                    autocomplete="lastname"
+                />
+
+                <InputError class="mt-2" :message="form.errors.lastname" />
+            </div>
+            <div>
+                <InputLabel for="address" value="address" />
+
+                <TextInput
+                    id="address"
+                    type="text"
+                    class="mt-1 block w-full"
+                    v-model="form.address"
+                    required
+                    autocomplete="address"
+                />
+
+                <InputError class="mt-2" :message="form.errors.address" />
+            </div>
+            <div>
+                <InputLabel for="birthday" value="birthday" />
+                <TextInput
+                    id="birthday"
+                    type="date"
+                    class="mt-1 block w-full"
+                    v-model="form.birthday"
+                    required
+                    autocomplete="birthday"
+                />
+
+                <InputError class="mt-2" :message="form.errors.lastname" />
+            </div>
+
+            <div
+                v-if="props.mustVerifyEmail && user.email_verified_at === null"
+            >
+                <p class="text-sm mt-2 text-gray-800">
+                    Your email address is unverified.
+                    <Link
+                        :href="route('verification.send')"
+                        method="post"
+                        as="button"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Click here to re-send the verification email.
+                    </Link>
+                </p>
+
+                <div
+                    v-show="props.status === 'verification-link-sent'"
+                    class="mt-2 font-medium text-sm text-green-600"
+                >
+                    A new verification link has been sent to your email address.
+                </div>
+            </div>
+
+            <div class="flex items-center gap-4">
+                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                <Transition
+                    enter-from-class="opacity-0"
+                    leave-to-class="opacity-0"
+                    class="transition ease-in-out"
+                >
+                    <p
+                        v-if="form.recentlySuccessful"
+                        class="text-sm text-gray-600"
+                    >
+                        Saved.
+                    </p>
+                </Transition>
+            </div>
+        </form>
+    </section>
+</template>
